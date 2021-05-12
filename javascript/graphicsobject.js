8ba8c329fd6fa5d0764dc552d542c35a617df70a
Wino99 <84073265+Wino99@users.noreply.github.com>
2021-05-12 12:24:50 +0200

    Delete javascript directory

--- javascript/graphicsobject.js
+++
@@ -1,9 +0,0 @@
-class GraphicsObject
-{
-	//innrhåller en samling av geometri och färgen den ska färgas
-	constructor(renderObject, color)
-	{
-		this.renderObject = renderObject;
-		this.color = color;
-	}
-}
